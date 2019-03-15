fbb729156481ee2ad2879dbcbc3ef548fab2d87c
Shinichi Nakajima <nakajimabb@gmail.com>
2019-03-16 01:47:49 +0900

    feat: add Article model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from core.models import TimeStampedModel, AuthModel
 
-# Create your models here.
+
+class Article(TimeStampedModel, AuthModel):
+    title = models.CharField(max_length=256, verbose_name='タイトル')
+    content = models.TextField(verbose_name='本文')
+    is_directory = models.BooleanField(default=False, verbose_name='ディレクトリ')
+    parent = models.ForeignKey('Article', null=True, blank=True, on_delete=models.SET_NULL)
+    headline = models.CharField(blank=True, default='', max_length=64, verbose_name='見出し')
+    permit_comment = models.BooleanField(default=False, verbose_name='コメント許可')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return "/articles/%i" % self.id
+
+    def parents(self, reverse=True):
+        p_list = []
+        p = self.parent
+        while p:
+            p_list.append(p)
+            p = p.parent
+        if reverse:
+            p_list.reverse()
+        return p_list
+
+    class Meta:
+        ordering = ['-is_directory']
--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('articles/', include('articles.urls')),
     path('', RedirectView.as_view(url='/accounts/home/')),
 ]
